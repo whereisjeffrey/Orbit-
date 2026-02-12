3575e8a80d73eeb2b4d5e36e288e00f310158864
whereisjeffrey <whereisjeffrey@gmail.com>
2026-02-12 07:39:41 -0300

    Initial Commit

---
+++ TranslateHelper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TranslateHelper
+//
+//  Created by Jeffrey Johnson on 12/02/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
